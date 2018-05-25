2bfbc98d740efd3ed619bf6ed793e47feabd19eb
chaoqun.wang <chaoqun.wang@ivglass.com>
2018-05-25 22:04:35 +0800

    x

--- js/main.js
+++ js/main.js
@@ -1,87 +1,149 @@
-jQuery(document).ready(function($) {
-
+;(function () {
+	
 	'use strict';
 
-        $(window).load(function() { // makes sure the whole site is loaded
-            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-        })
-      
-        
-        $(function() {
-  
-        function showSlide(n) {
-            // n is relative position from current slide
-          
-            // unbind event listener to prevent retriggering
-            $body.unbind("mousewheel");
-          
-            // increment slide number by n and keep within boundaries
-            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
-            
-            var displacment = window.innerWidth*currSlide;
-            // translate slides div across to appropriate slide
-            $slides.css('transform', 'translateX(-' + displacment + 'px)');
-            // delay before rebinding event to prevent retriggering
-            setTimeout(bind, 700);
-            
-            // change active class on link
-            $('nav a.active').removeClass('active');
-            $($('a')[currSlide]).addClass('active');
-            
-        }
-      
-        function bind() {
-             $body.bind('false', mouseEvent);
-          }
-      
-        function mouseEvent(e, delta) {
-            // On down scroll, show next slide otherwise show prev slide
-            showSlide(delta >= 0 ? -1 : 1);
-            e.preventDefault();
-        }
-        
-        $('nav a, .main-btn a').click(function(e) {
-            // When link clicked, find slide it points to
-            var newslide = parseInt($(this).attr('href')[1]);
-            // find how far it is from current slide
-            var diff = newslide - currSlide - 1;
-            showSlide(diff); // show that slide
-            e.preventDefault();
-        });
-      
-        $(window).resize(function(){
-          // Keep current slide to left of window on resize
-          var displacment = window.innerWidth*currSlide;
-          $slides.css('transform', 'translateX(-'+displacment+'px)');
-        });
-        
-        // cache
-        var $body = $('body');
-        var currSlide = 0;
-        var $slides = $('.slides');
-        var $slide = $('.slide');
-      
-        // give active class to first link
-        $($('nav a')[0]).addClass('active');
-        
-        // add event listener for mousescroll
-        $body.bind('false', mouseEvent);
-    })        
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	
+	var fullHeight = function() {
+
+		if ( !isMobile.any() ) {
+			$('.js-fullheight').css('height', $(window).height());
+			$(window).resize(function(){
+				$('.js-fullheight').css('height', $(window).height());
+			});
+		}
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn animated-fast');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated-fast');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated-fast');
+							} else {
+								el.addClass('fadeInUp animated-fast');
+							}
+
+							el.removeClass('item-animate');
+						},  k * 100, 'easeInOutExpo' );
+					});
+					
+				}, 50);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
 
 
-        $('#form-submit .date').datepicker({
-        });
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+
+	var pieChart = function() {
+		$('.chart').easyPieChart({
+			scaleColor: false,
+			lineWidth: 4,
+			lineCap: 'butt',
+			barColor: '#FF9000',
+			trackColor:	"#f5f5f5",
+			size: 160,
+			animate: 1000
+		});
+	};
+
+	var skillsWayPoint = function() {
+		if ($('#fh5co-skills').length > 0 ) {
+			$('#fh5co-skills').waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+					setTimeout( pieChart , 400);					
+					$(this.element).addClass('animated');
+				}
+			} , { offset: '90%' } );
+		}
+
+	};
 
 
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
-            }
-        });
+	// Loading page
+	var loaderPage = function() {
+		$(".fh5co-loader").fadeOut("slow");
+	};
+
+	
+	$(function(){
+		contentWayPoint();
+		goToTop();
+		loaderPage();
+		fullHeight();
+		parallax();
+		// pieChart();
+		skillsWayPoint();
+	});
 
 
-});
+}());
